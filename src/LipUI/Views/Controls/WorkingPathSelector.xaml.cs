5b2ca0d03b5ffce62026b71da3a3b714d2cc8edb
LazuliKao <lazulikao@nbt.edu.cn>
2023-02-14 21:52:36 +0800

    fix bug

--- src/LipUI/Views/Controls/WorkingPathSelector.xaml.cs
+++ src/LipUI/Views/Controls/WorkingPathSelector.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using LipUI.ViewModels;
 
 namespace LipUI.Views.Controls
 {
@@ -20,9 +8,11 @@ namespace LipUI.Views.Controls
     /// </summary>
     public partial class WorkingPathSelector : UserControl
     {
+        public WorkingPathSelectorViewModel ViewModel => (WorkingPathSelectorViewModel)DataContext;
         public WorkingPathSelector()
         {
             InitializeComponent();
+            this.DataContext = new WorkingPathSelectorViewModel();
         }
     }
 }
